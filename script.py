87a502624c6e43c651ef8cb5d788dbc3cbbebb71
V0Iek <michal.bernas2005@gmail.com>
2023-09-28 20:43:19 +0200

    Added some comments to script

--- script.py
+++ script.py
@@ -3,25 +3,28 @@ import os
 from PIL import Image
 from PIL.ExifTags import TAGS
 
+# Create list with .jpg files in current dir
 files = []
 
 for file in os.listdir():
     if file.endswith(".jpg"):
         files.append(file)
 
+
 for file in files:
+    # Get metadata from file
     image = Image.open(file)
     exifdata = image.getexif()
 
+    # Get data from metadata
     date = exifdata.get(306)
     date = date[:10]
 
-    #print("-----", file, "-----")
-    #print("Date: ", date)
-
+    # Create dir with date if not exist
     if not os.path.isdir(date):
         os.mkdir(os.path.join(os.getcwd(), date))
 
+    # Move image to folder with its date
     source = os.path.join(os.getcwd(), file)
     dest = os.path.join(os.path.join(os.getcwd(), date), file)
 
