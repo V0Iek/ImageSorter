6b4ace83d9cb96083f2b7261238ecc8b4239f65a
V0Iek <michal.bernas2005@gmail.com>
2023-09-28 20:30:18 +0200

    Script now can sort images by date in its metadata

--- script.py
+++ script.py
@@ -13,17 +13,16 @@ for file in files:
     image = Image.open(file)
     exifdata = image.getexif()
 
-    #for tagid in exifdata:
-    #    tagname = TAGS.get(tagid, tagid)
-    #    value = exifdata.get(tagid)
-
-    #    if "Date" in tagname:
-    #        print(tagid)
-    #        print(value)
-
     date = exifdata.get(306)
-
     date = date[:10]
 
-    print("-----", file, "-----")
-    print("Date: ", date)
+    #print("-----", file, "-----")
+    #print("Date: ", date)
+
+    if not os.path.isdir(date):
+        os.mkdir(os.path.join(os.getcwd(), date))
+
+    source = os.path.join(os.getcwd(), file)
+    dest = os.path.join(os.path.join(os.getcwd(), date), file)
+
+    os.rename(source, dest)
