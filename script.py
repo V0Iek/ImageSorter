72def822c02d3176863eb11d662a2c904944882d
V0Iek <michal.bernas2005@gmail.com>
2023-09-28 17:24:10 +0200

    Added extracting metadata from images

--- script.py
+++ script.py
@@ -1,12 +1,20 @@
 import os
 
+from PIL import Image
+from PIL.ExifTags import TAGS
+
 files = []
 
 for file in os.listdir():
-    if file.endswith(".png"):
-        print(file, " is image")
+    if file.endswith(".jpg"):
         files.append(file)
-    else:
-        print(file, " isn't image")
 
-print(files)
+for file in files:
+    image = Image.open(file)
+    exifdata = image.getexif()
+
+    for tagid in exifdata:
+        tagname = TAGS.get(tagid, tagid)
+        value = exifdata.get(tagid)
+
+        print(f"{tagname:25}: {value}")
